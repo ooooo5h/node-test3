cf58b0119ac22d5869dc46aad41d07dd4a95cf84
eunhyung <ooooo5h91@gmail.com>
2022-08-18 21:06:10 +0900

    특정 유저 목록 가져오기

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,16 @@
 const router = require("express").Router();
 const db = require("../config/mysql");
 
-router.post("/", async(req, res, next) => {
-    try {
-        const sql =
-        `
+router.post("/", async (req, res, next) => {
+  try {
+    const sql = `
         insert into users (user_id, name, age) values (${req.body.user_id}, '${req.body.name}', ${req.body.age});
-        `
-        await db.query(sql)
-        return res.status(200).json({message:"success"});
-    } catch(e) {
-        console.log(e.message);
-    }
+        `;
+    await db.query(sql);
+    return res.status(200).json({ message: "success" });
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 // 안되는 코드 => 이대로 테스트하면 뒤에 buff라고 해서 이상한 데이터들이 붙어온다.
@@ -26,16 +25,15 @@ router.post("/", async(req, res, next) => {
 // }
 
 // 되는 코드 1 => 인덱스로 짤라내면 된다.
-router.get("/", async(req, res, next) => {
-    try {
-        const sql = 'select * from users'
-        const result = await db.query(sql);
-        return res.status(200).json({message:"success", 'result':result[0]})
-    } catch(e) {
-        console.log(e.message);
-    }
-})
-
+router.get("/", async (req, res, next) => {
+  try {
+    const sql = "select * from users";
+    const result = await db.query(sql);
+    return res.status(200).json({ message: "success", result: result[0] });
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
 // 되는 코드2
 // router.get("/", async (req, res, next) => {
@@ -46,5 +44,25 @@ router.get("/", async(req, res, next) => {
 //     console.log(e.message);
 //   }
 // });
+
+// 특정 유저 정보 가져오기
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const userId         = req.params.userId;        
+    const sql            = `select * from users where user_id=${userId}`;
+    const [rows, fields] = await db.query(sql);
+    // console.log(rows)
+    if (rows.length) {
+        // console.log('데이터있음')
+        return res.status(200).json({ message: "success", result: rows });
+    } else {
+        // console.log('데이터없음')
+        return res.status(404).json({message:"USER_NOT_FOUND"})
+    }
+    
+  } catch (e) {
+    console.log(e.message);
+  }
+});
 
 module.exports = router;
