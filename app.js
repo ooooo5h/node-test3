fbb7102e5042962c307129d1e04fb7707b493c7c
eunhyung <ooooo5h91@gmail.com>
2022-08-19 22:05:23 +0900

    app.js에서 routes로 보내기

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(routes);
 
 app.get('/', (req,res) => {
     res.send('hello node')
